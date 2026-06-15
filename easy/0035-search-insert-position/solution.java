90b64228b071d3b2c98719ffeae983bfcf3177d0
Shravan Navale <shravan.n.024@gmail.com>
2026-06-15 21:36:07 +0530

    ✅ Search Insert Position | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 44.6 MB | 2026-06-15

--- easy/0035-search-insert-position/solution.java
+++ easy/0035-search-insert-position/solution.java
@@ -1,11 +1,19 @@
-                return mid;
-            }else if(nums[mid] < target){
-                start = mid+1;
-            if(nums[mid] == target){
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
 
-            }else{
-                end = mid-1;
-            }
-        }
-        return start;
-    }
+        while(start<=end){
+           int mid = (start+end)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return start;
+    }
+}
