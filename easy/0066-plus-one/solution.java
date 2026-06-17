e8f0f87bd1d66986f5ed5e8aa526d745dfbedc6c
Shravan Navale <shravan.n.024@gmail.com>
2026-06-17 20:55:26 +0530

    ✅ Plus One | Easy | Java | Runtime: 0 ms | Memory: 43 MB | 2026-06-17

--- easy/0066-plus-one/solution.java
+++ easy/0066-plus-one/solution.java
@@ -1,10 +1,10 @@
-class Solution {
-    public int[] plusOne(int[] digits) {
-        for(int i = 0; i < digits.length-1; i++){
-            if(digits[i] == digits.length-1){
-                digits[i]+=1;
-            }
+
+            digits[i] = 0;
         }
-        return digits;
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
     }
 }
