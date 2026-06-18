e3669544280942551ee4e245f1a865a1cd46565e
Shravan Navale <shravan.n.024@gmail.com>
2026-06-18 23:17:47 +0530

    ✅ Find First and Last Position of Element in Sorted Array | Medium | Java | Runtime: 12 ms (0.88%) | Memory: 48 MB | 2026-06-18

--- medium/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ medium/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,10 +1,48 @@
-        }
-        else if(demo.size()>1)
-        {
-            ans[0]=demo.get(0);
-            ans[1]=demo.get(demo.size()-1);
-        }
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        
+        int n=nums.length;
 
-        return ans;
-    }
+        Map<Integer, List<Integer>> mp= new HashMap<>();
+
+        int ans[]= new int[2];
+
+        Arrays.fill(ans, -1);
+
+        //if(n==1) return new int[]{0,0};
+        if(n==0) return ans;
+
+        for(int i=0; i<n; i++)
+        {
+            if(!mp.containsKey(nums[i]))
+            {
+                mp.put(nums[i], new ArrayList<Integer>());
+            }
+
+            List<Integer> samp=mp.get(nums[i]);
+            samp.add(i);
+
+            mp.put(nums[i], samp);
+        }
+
+        if(!mp.containsKey(target))
+        {
+            return ans;
+        }
+
+        List<Integer> demo=mp.get(target);
+
+        if(demo.size()==1)
+        {
+            ans[0]=demo.get(0);
+            ans[1]=demo.get(0);
+        }
+        else if(demo.size()>1)
+        {
+            ans[0]=demo.get(0);
+            ans[1]=demo.get(demo.size()-1);
+        }
+
+        return ans;
+    }
 }
