5635a2dc975a827d1e62e92e53696452816fb55c
Shravan Navale <shravan.n.024@gmail.com>
2026-06-19 21:21:31 +0530

    ✅ Find the Highest Altitude | Easy | Java | Runtime: 0 ms | Memory: 42 MB | 2026-06-19

--- easy/1732-find-the-highest-altitude/solution.java
+++ easy/1732-find-the-highest-altitude/solution.java
@@ -1,9 +1,11 @@
-        int max = 0;
 
-        for(int i = 0; i < gain.length; i++){
+        for(int i = 0; i < gain.length-1; i++){
             current += i;
         }
-    }
             max = Math.max(max, current);
         return max;
-}
+        int max = 0;
+        int current = 0;
+    public int largestAltitude(int[] gain) {
+    }
+class Solution {
